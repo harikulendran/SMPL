45206f08fad1e1a881c5b6391a9bb790c8760c3d
Hari <hk1n15@ecs.soton.ac.uk>
2018-03-14 14:55:46 +0000

    an implementation of equals

--- Evaluator.hs
+++ Evaluator.hs
@@ -4,7 +4,8 @@ import Tokens
 import Grammar
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text
-import Data.List (sort)
+import Data.List (sort, group, reverse)
+import Data.Char (isAlpha, isNumber)
 
 type Scope = [(Int,String)]
 
@@ -47,9 +48,29 @@ update scope val =
                            | otherwise      = update' ss v (m++[s])
     in update' scope val []
 
+updateAll :: Scope -> (Int,String) -> Scope
+updateAll scope val = 
+    let update'     [] _ m                  = m
+        update' (s:ss) v m | fst s == fst v = m ++ [v] ++ (update' ss v [])
+                           | otherwise      = update' ss v (m++[s])
+    in update' scope val []
+
+renameV scope val val2 = 
+    let update'     [] _  _ m                  = m
+        update' (s:ss) v v2 m | fst s == fst v = m ++ [v2] ++ (update' ss v v2 [])
+                              | otherwise      = update' ss v v2 (m++[s])
+    in update' scope val val2 []
+
 equiv :: [String] -> Bool
 equiv xs = (filter (\n -> n /= (head xs)) xs) == []
 
+(===) :: String -> String -> Bool
+(===) s1 s2 =
+    let testString s = filter (\n -> n=='.' || isAlpha n) s
+    in (testString s1) == (testString s2)
+
+lzip (x:xs) (y:ys) = (x++[y]):lzip xs ys
+lzip      _      _ = []
 
 
 
@@ -64,18 +85,23 @@ filterScope s1 (s:s2) out | s1 `contains` (fst s) = filterScope s1 s2 (out++[(fs
 filterByExist :: Scope -> Scope -> (Scope,Scope)
 filterByExist   []     _ = ([],[])
 filterByExist vars scope | (and $ map (\n -> scope `contains` (fst n)) vars)  = (scope,[])
-                         | otherwise                                          = ([],[]) 
+                         | otherwise                                          = ([],[])
+
+filterByEqual :: Scope -> (Int,String) -> (Int,String) -> (Scope,Scope)
+filterByEqual [] _ _      = ([],[])
+filterByEqual scope v1 v2 = ((scope `updateAll` (fst v1,(find (fst v1) scope)++"="++(show $ (fst v2)))) `updateAll` (fst v2,(find (fst v2) scope)++"="++(show $ (fst v1))),[])
 
 
     
 eval' :: Scope -> Expr -> String  -> (Scope,Scope)
 eval' scope expr str  = case expr of
-    Var i             -> (scope ++ [(i,str)],[])
-    Comma e1 e2       -> ((fst(eval' scope e1 str)) ++ (fst(eval' scope e2 str)),[])
-    For e s           -> ((0,s):(fst(eval' scope e s)),[])
-    IfExist e1 e2     -> filterByExist (fst $ eval' scope e1 str) (fst $ eval' scope e2 str)
-    And e1 e2         -> ((fst $ eval' scope e1 str) ++ (fst $ eval' scope e2 str),[])
-    Do e1 e2          -> filterScope (fst(eval' scope e1 str)) ((fst(eval' scope e2 str))) []
+    Var i                -> (scope ++ [(i,str)],[])
+    Comma e1 e2          -> ((fst(eval' scope e1 str)) ++ (fst(eval' scope e2 str)),[])
+    For e s              -> ((0,s):(fst(eval' scope e s)),[])
+    IfExist e1 e2        -> filterByExist (fst $ eval' scope e1 str) (fst $ eval' scope e2 str)
+    And e (Equals v1 v2) -> filterByEqual (fst $ eval' scope e str) (head $ fst $ eval' scope v1 str) (head $ fst $ eval' scope v2 str)
+    And e1 e2            -> ((fst $ eval' scope e1 str) ++ (fst $ eval' scope e2 str),[])
+    Do e1 e2             -> filterScope (fst(eval' scope e1 str)) ((fst(eval' scope e2 str))) []
 
 
 
@@ -83,9 +109,9 @@ eval' scope expr str  = case expr of
 -- Reading Files
 commaSplit :: Text.Text -> String -> Scope -> Scope
 commaSplit iline name scope =
-    let checkVar []                       = -1
-        checkVar (v:vars) | snd v == name = fst v
-                          | otherwise     = checkVar vars
+    let checkVar []                        = -1
+        checkVar (v:vars) | snd v === name = fst v
+                          | otherwise      = checkVar vars
 
         getWord []       cWord out = out++[cWord]
         getWord (l:line) cWord out | l == ','  = getWord line [] (out++[cWord])
@@ -126,10 +152,27 @@ eval (scope,vars) = do
                                   | otherwise      = []
 
         varsEquiv scp = and $ map (\var -> (equiv $ scp `findAll` (fst var))) scp
+        
+        --Get substitution rules
+        subRules scope = lzip (map head (group $ sort $ map (\n -> sort [fst n, read (filter isNumber (snd n))::Int]) (filter (\n -> '=' `elem` (snd n)) scope))) [1..]
 
-        orderLine line = orderLine' vars line
+        --Get next free variable
+        getBndVars scp = map head (group $ sort $ map fst scp)
+        getFreeVar xs = (last xs) + 1
+        free  = getFreeVar $ getBndVars (scope++vars)
 
-    return $ unlines $ sort $ filter (\n -> n /= []) (map orderLine fdata)
+        --Rename variable to next free var
+        rename' scp x y = renameV scp (x,"") (y,find x scp)
+        rename scp x y m = rename' (rename' scp x ((free ) + m)) y ((free ) + m)
+
+        --Apply substitutions
+        cleanLine s [] = s
+        cleanLine s (([a,b,c]):rs) = cleanLine (rename s a b c) rs
+        cleanData fdata scope = map (\n -> cleanLine (fst n) (subRules (snd n))) (zip fdata (repeat scope))
+
+        orderLine line = orderLine' (head $ cleanData [vars] scope) line
+
+    return $ unlines $ sort $ filter (\n -> n /= []) (map orderLine (cleanData fdata scope))
 
 
 
